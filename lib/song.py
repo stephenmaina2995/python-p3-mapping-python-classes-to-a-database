1b760841bec878e3ab68dd75f347ba9cdeaa238c
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-05-22 14:32:48 +0300

    Completed assignment

--- lib/song.py
+++ lib/song.py
@@ -1,19 +1,31 @@
 from config import CONN, CURSOR
 
+import sqlite3
+
+# Create a connection to the database
+conn = sqlite3.connect('music.db')
+CURSOR = conn.cursor()
+
 class Song:
     def __init__(self, name, album):
-        self.id = None
         self.name = name
         self.album = album
 
     @classmethod
-    def create_table(self):
-        sql = """
-            CREATE TABLE IF NOT EXISTS songs (
-                id INTEGER PRIMARY KEY,
-                name TEXT,
-                album TEXT
-            )
-        """
+    def create_table(cls):
+        CURSOR.execute('''CREATE TABLE IF NOT EXISTS songs (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            name TEXT,
+                            album TEXT
+                        )''')
+        conn.commit()
 
-        CURSOR.execute(sql)
+    def save(self):
+        CURSOR.execute('INSERT INTO songs (name, album) VALUES (?, ?)', (self.name, self.album))
+        conn.commit()
+
+    @classmethod
+    def create(cls, name, album):
+        song = cls(name, album)
+        song.save()
+        return song
